7889680bd982d2e4770699382098c64ef446ad50
Burner <burner@burner.gay>
2022-10-24 16:07:15 -0500

    Only forward the content-type sticker header

--- src/sticker-api-helper.ts
+++ src/sticker-api-helper.ts
@@ -5,8 +5,12 @@ export async function getSticker<T extends string>(
   name: string,
 ) {
   const res = await fetch(`${ctx.env.STICKER_API_BASE}/glitchfur/${name}`)
-  if (res.status === 200) {
-    return res
+  const { status } = res
+  if (status === 200) {
+    const headers = new Headers({
+      'Content-Type': res.headers.get('Content-Type') as string,
+    })
+    return new Response(res.body, { status: status, headers: headers })
   }
   return null
 }
